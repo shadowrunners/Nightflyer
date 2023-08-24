ad32bcea29044d6bd17ec81ed77b704424c61ddb
scrappie <scr3ppie@gmail.com>
2023-08-24 17:36:07 +0300

    refactor(auth): replaced url constant with string

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -1,21 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { CLIENT_ID } from '@/utils/auth/server';
-import { getAbsoluteUrl } from '@/utils/get-absolute-url';
+import { getAbsoluteUrl } from '@/utils/utils';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { locale } = req.query as {
+	const { locale } = req.query as {
     locale?: string;
   };
 
-  const url =
-    'https://discord.com/api/oauth2/authorize?' +
-    new URLSearchParams({
-      client_id: CLIENT_ID,
-      redirect_uri: `${getAbsoluteUrl()}/api/auth/callback`,
-      response_type: 'code',
-      scope: 'identify guilds',
-      state: locale ?? '',
-    });
-
-  res.redirect(302, url);
+	return res.redirect(302, `https://discord.com/oauth2/authorize?client_id=${process.env.CLIENT_ID}?redirect_uri=${getAbsoluteUrl()}/api/auth/callback?response_type=code?scope=identify+guilds?state=${locale ?? ''}`);
 }
