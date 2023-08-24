239b724ebc6692546f702b128d5d12030a04ac4e
scrappie <scr3ppie@gmail.com>
2023-08-24 17:24:06 +0300

    refactor(tailwind): added config from main website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+	content: [
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
 
-    // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {},
-    screens: {
-      xs: '480px',
-      ss: '620px',
-      sm: '768px',
-      md: '1060px',
-      lg: '1200px',
-      xl: '1700px',
-    },
-  },
-  plugins: [],
+		// Or if using `src` directory:
+		'./src/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	mode: 'jit',
+	theme: {
+		extend: {
+			colors: {
+				primary: '#00040f',
+				secondary: '#00f6ff',
+				dimWhite: 'rgba(255, 255, 255, 0.7)',
+				dimBlue: 'rgba(9, 151, 124, 0.1)',
+			},
+			fontFamily: {
+				poppins: ['Poppins', 'sans-serif'],
+			},
+		},
+		screens: {
+			xs: '480px',
+			ss: '620px',
+			sm: '768px',
+			md: '1060px',
+			lg: '1200px',
+			xl: '1700px',
+		},
+	},
+	plugins: [],
 };
