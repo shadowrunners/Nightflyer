5daad759a43feb3166cf91d28bd761f511859e8c
scrappie <scr3ppie@gmail.com>
2023-08-24 17:22:32 +0300

    feat(utils): added utils + more types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,1 +1,22 @@
+import {
+	WelcomeFeature,
+	AntiPhishingFeature,
+	ConfessionsFeature,
+	GoodbyeFeature,
+	LogsFeature,
+	LevellingFeature,
+	TicketsFeature,
+	VerifyFeature,
+} from '@/config/types';
+
+export type Features =
+	WelcomeFeature |
+	AntiPhishingFeature |
+	ConfessionsFeature |
+	GoodbyeFeature |
+	LogsFeature |
+	LevellingFeature |
+	TicketsFeature |
+	VerifyFeature;
+
 export type Override<T, R> = Omit<T, keyof R> & R;
---
+++ src/utils/utils.ts
@@ -0,0 +1,59 @@
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import { getServerSession } from 'next-auth/next';
+
+/** Styles used in the main website. */
+
+export type Styles = {
+	boxWidth: string;
+	boxNav: string;
+	heading2: string;
+	paragraph: string;
+	flexCenter: string;
+	flexStart: string;
+	paddingX: string;
+	paddingY: string;
+	padding: string;
+	marginX: string;
+	marginY: string;
+};
+
+export const styles: Styles = {
+	boxWidth: 'xl:max-w-[1280px] w-full',
+	boxNav: 'xl:max-w-[1920px] w-full',
+
+	heading2:
+		'font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full',
+	paragraph:
+		'font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]',
+
+	flexCenter: 'flex justify-center items-center',
+	flexStart: 'flex justify-center items-start',
+
+	paddingX: 'sm:px-16 px-6',
+	paddingY: 'sm:py-16 py-6',
+	padding: 'sm:px-16 px-6 sm:py-12 py-4',
+
+	marginX: 'sm:mx-16 mx-6',
+	marginY: 'sm:my-16 my-6',
+};
+
+export const layout = {
+	section: `flex md:flex-row flex-col ${styles.paddingY}`,
+	sectionReverse: `flex md:flex-row flex-col-reverse ${styles.paddingY}`,
+
+	sectionImgReverse: `flex-1 flex ${styles.flexCenter} md:mr-20 mr-0 md:mt-0 mt-10 relative`,
+	sectionImg: `flex-1 flex ${styles.flexCenter} md:ml-10 ml-0 md:mt-0 mt-10 relative`,
+
+	sectionInfo: `flex-1 ${styles.flexStart} flex-col`,
+};
+
+/**
+ * Gets the URL of the callback URL.
+ * @returns The callback URL.
+ */
+export function getAbsoluteUrl(): string {
+	const defaultUrl = 'http://localhost:3000';
+
+	if (process.env.APP_URL !== null) return process.env.APP_URL as string;
+	return process.env.VERCEL_URL === null ? defaultUrl : `https://${process.env.VERCEL_URL}`;
+}
