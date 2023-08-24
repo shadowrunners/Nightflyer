a1258f3952543c730241c81c9bcbca4446695bd3
scrappie <scr3ppie@gmail.com>
2023-08-24 17:23:41 +0300

    refactor(layout): removed morefeatures

--- src/components/layout/main/index.ts
+++ src/components/layout/main/index.ts
@@ -1,9 +1,8 @@
-import Navbar from "./Navbar";
-import Features from "./Features";
-import Footer from "./Footer";
-import Developers from "./Developers";
-import Hero from "./Hero";
-import Stats from "./Stats";
-import MoreFeatures from "./MoreFeatures";
+import Navbar from './Navbar';
+import Features from './Features';
+import Footer from './Footer';
+import Developers from './Developers';
+import Hero from './Hero';
+import Stats from './Stats';
 
-export { Developers, Features, Footer, Hero, Navbar, MoreFeatures, Stats };
+export { Developers, Features, Footer, Hero, Navbar, Stats };
