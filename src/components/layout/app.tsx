eef0681cf3943f84c146c656d6fc84dbfc167d67
scrappie <scr3ppie@gmail.com>
2023-08-25 13:37:31 +0300

    refactor(layout): added querystatus back to app.tsx

--- src/components/layout/app.tsx
+++ src/components/layout/app.tsx
@@ -25,38 +25,41 @@ export default function AppLayout({
 			<Show below={sidebarBreakpoint}>
 				<SidebarResponsive sidebar={sidebar} />
 			</Show>
-			<Flex
-				pos="relative"
-				direction="column"
-				height="100%"
-				overflow="auto"
-				w="full"
-				maxWidth={{ base: '100%', xl: 'calc( 100% - 290px )' }}
-				maxHeight="100%"
-			>
-				<Box
-					top={0}
-					mx="auto"
-					maxW="1200px"
-					zIndex="sticky"
-					pos="sticky"
+			<QueryStatus query={query} loading={<LoadingPanel />} error="Couldn't fetch the necessary data about the user.">
+				<Flex
+					pos="relative"
+					direction="column"
+					height="100%"
+					overflow="auto"
 					w="full"
-					pt={{ [navbarBreakpoint]: '16px' }}
-					px={{ '3sm': '30px' }}
+					maxWidth={{ base: '100%', xl: 'calc( 100% - 290px )' }}
+					maxHeight="100%"
 				>
-					<Navbar>{navbar ?? <DefaultNavbar />}</Navbar>
-				</Box>
-				<Box
-					mx="auto"
-					w="full"
-					maxW="1200px"
-					flex={1}
-					my={{ base: '30px', [sidebarBreakpoint]: '50px' }}
-					px={{ base: '24px', '3sm': '30px' }}
-				>
-					{children}
-				</Box>
-			</Flex>
+					<Box
+						top={0}
+						mx="auto"
+						maxW="1200px"
+						zIndex="sticky"
+						pos="sticky"
+						w="full"
+						pt={{ [navbarBreakpoint]: '16px' }}
+						px={{ '3sm': '30px' }}
+					>
+						<Navbar>{navbar ?? <DefaultNavbar />}</Navbar>
+					</Box>
+					<Box
+						mx="auto"
+						w="full"
+						maxW="1200px"
+						flex={1}
+						my={{ base: '30px', [sidebarBreakpoint]: '50px' }}
+						px={{ base: '24px', '3sm': '30px' }}
+					>
+						{children}
+					</Box>
+				</Flex>
+			</QueryStatus>
 		</Flex>
+
 	);
 }
