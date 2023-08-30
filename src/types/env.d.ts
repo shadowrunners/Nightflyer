d283660d2bdcc8534192ef99b45379403a629ee6
scrappie <scr3ppie@gmail.com>
2023-08-31 02:13:47 +0300

    feat(types): added env types

---
+++ src/types/env.d.ts
@@ -0,0 +1,26 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      /** The bot's client ID. */
+      CLIENT_ID: string;
+      /** The bot's client secret. */
+      CLIENT_SECRET: string;
+      /** The URL to where the dashboard is hosted. */
+      APP_URL: string;
+      /** The URL of the dashboard. */
+      NEXTAUTH_URL: string;
+      /** The secret used to encrypt / decrypt JWT tokens. */
+      NEXTAUTH_SECRET: string;
+      /** The URL to the dashboard's API. */
+      API_URL: string;
+
+      /**
+       * The url where the frontend is hosted
+       *
+       * ex: `https://my-bot.vercel.app`, default: `http://localhost:3000`
+       */
+      WEB_URL?: string;
+    }
+  }
+}
+export {};
