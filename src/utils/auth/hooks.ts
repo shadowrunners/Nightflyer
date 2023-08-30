88902227418cf4c4473d78eb321fe3cc2a9f23b9
scrappie <scr3ppie@gmail.com>
2023-08-31 02:14:31 +0300

    refactor(auth): removed old auth utils

--- src/utils/auth/hooks.ts
+++ src/utils/auth/hooks.ts
@@ -1,19 +1,7 @@
-import { useMutation, useQuery } from '@tanstack/react-query';
-import { client, Keys } from '@/api/hooks';
-import { AccessToken } from './server';
-import { callReturn, callDefault } from '@/utils/fetch/core';
+import { useMutation } from '@tanstack/react-query';
+import { callDefault } from '@/utils/fetch/core';
+import { useSession } from 'next-auth/react';
 import Router from 'next/router';
-
-/**
- * Get discord oauth2 access token if logged in, otherwise return null
- */
-async function auth() {
-	return await callReturn<AccessToken>('/api/auth', {
-		request: {
-			method: 'GET',
-		},
-	});
-}
 
 export async function logout() {
 	await callDefault('/api/auth/signout', {
@@ -22,45 +10,12 @@ export async function logout() {
 		},
 	});
 
-	await client.invalidateQueries(Keys.login);
 	await Router.push('/landing');
 }
 
-type SessionResult =
-  | {
-      status: 'authenticated';
-      session: AccessToken;
-    }
-  | {
-      status: 'loading' | 'unauthenticated';
-      session: null;
-    };
-
-export function useSession(): SessionResult {
-	const { isError, isLoading, data } = useQuery(Keys.login, () => auth());
-
-	if (isError)
-		return {
-			status: 'unauthenticated',
-			session: null,
-		};
-
-	if (isLoading)
-		return {
-			status: 'loading',
-			session: null,
-		};
-
-	return {
-		status: 'authenticated',
-		session: data,
-	};
-}
-
 export function useAccessToken() {
-	const { session } = useSession();
-
-	return session?.access_token;
+	const { data: session } = useSession();
+	return session?.accessToken;
 }
 
 export function useLogoutMutation() {
