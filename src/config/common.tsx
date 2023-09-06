d7921fb5b71c88496d647022ff0a2385509e07af
scrappie <notscrappie@proton.me>
2023-09-06 17:52:40 +0300

    refactor(config): updated invite url

--- src/config/common.tsx
+++ src/config/common.tsx
@@ -23,7 +23,7 @@ export const config: AppConfig = {
 	name: 'Evelyn',
 	icon: BotIcon,
 	inviteUrl:
-		'https://discord.com/oauth2/authorize?client_id=1070011901385375845&permissions=8&scope=bot',
+		'https://discord.com/oauth2/authorize?client_id=1104526040656785418&scope=bot',
 	guild: {
 		// filter guilds that user has no permissions to manage it
 		filter: (guild) => (Number(guild.permissions) & PermissionFlags.ADMINISTRATOR) !== 0,
