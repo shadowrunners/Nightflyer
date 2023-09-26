d5ee0bb7be8fd3db3817f90c16d0da9c79a37100
scrappie <notscrappie@proton.me>
2023-09-26 16:46:23 +0300

    feat(utils): added guild + channel fetching

--- src/utils/discord.ts
+++ src/utils/discord.ts
@@ -1,5 +1,19 @@
+import { GuildChannel } from '@/types/types';
+
 export async function fetchGuild(id: string) {
 	const data = await fetch(`https://discord.com/api/v10/guilds/${id}`, {
+		method: 'GET',
+		headers: {
+			Authorization: `Bot ${process.env.CLIENT_TOKEN as string}`,
+		},
+	});
+
+	return await data.json();
+}
+
+export async function fetchChannels(id: string): Promise<GuildChannel[]> {
+	const data = await fetch(`https://discord.com/api/v10/guilds/${id}/channels`, {
+		method: 'GET',
 		headers: {
 			Authorization: `Bot ${process.env.CLIENT_TOKEN as string}`,
 		},
