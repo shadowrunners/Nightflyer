d91924ea1a58e23990ce29afec8138c86455399e
scrappie <notscrappie@proton.me>
2023-09-26 16:44:27 +0300

    refactor(channels): channels route returns channels

--- src/app/api/[locale]/guilds/[id]/channels/route.ts
+++ src/app/api/[locale]/guilds/[id]/channels/route.ts
@@ -4,10 +4,12 @@ import { NextResponse } from 'next/server';
 export async function GET(req: Request) {
 	try {
 		const url = new URL(req.url);
-		const id = url.pathname.split('/').pop();
-		const data = await fetchChannels(id as string);
+		const id = url.pathname.split('/')[4];
 
-		return NextResponse.json({ data });
+		if (id.length !== 18) return NextResponse.json({ err: 'Couldn\'t fetch the channels for this guild.' }, { status: 404 });
+
+		const data = await fetchChannels(id as string);
+		return NextResponse.json(data);
 	}
 	catch (_err) {
 		console.log(_err);
