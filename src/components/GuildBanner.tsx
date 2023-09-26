36e211a62fb537de5b892d4fd300967ecf88154a
scrappie <notscrappie@proton.me>
2023-09-26 16:45:24 +0300

    refactor(banner): added more margin to the top

--- src/components/GuildBanner.tsx
+++ src/components/GuildBanner.tsx
@@ -4,7 +4,7 @@ export function Banner() {
 	const t = useTranslations('dash');
 
 	return (
-		<div className='flex flex-col px-5 lg:px-8 py-5 lg:py-7 black2 rounded-2xl bg-cover gap-1'>
+		<div className='flex flex-col px-5 lg:px-8 mt-5 py-5 lg:py-7 black2 rounded-2xl bg-cover gap-1'>
 			<h1 className='text-white text-2xl font-bold'>
 				{t('banner.title')}
 			</h1>
