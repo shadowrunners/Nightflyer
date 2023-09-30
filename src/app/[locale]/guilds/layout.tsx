b8003334960d071765b58349a5768ba2c5c014c2
scrappie <notscrappie@proton.me>
2023-09-30 15:52:36 +0300

    fix(guild layout): alternate sidebar now works

--- src/app/[locale]/guilds/layout.tsx
+++ src/app/[locale]/guilds/layout.tsx
@@ -1,17 +1,15 @@
+'use client';
+
 import { InGuildSidebar } from '@/components/layout/guild/guild-sidebar';
 import GuildNavbar from '@/components/layout/guild/guild-navbar';
+import { usePathname } from 'next/navigation';
 import AppLayout from '../home/layout';
 import type { ReactNode } from 'react';
 
-export default function GuildLayout({
-	back,
-	children,
-}: {
-  back?: boolean;
-  children: ReactNode;
-}) {
+export default function GuildLayout({ children }: { children: ReactNode }) {
+	const back = usePathname().split('/')[4] ? true : false;
 	return (
-		<AppLayout navbar={<GuildNavbar back={back} />} sidebar={back ? <InGuildSidebar /> : undefined}>
+		<AppLayout navbar={<GuildNavbar />} sidebar={back ? <InGuildSidebar /> : undefined}>
 			{children}
 		</AppLayout>
 	);
--- src/components/layout/guild/guild-sidebar.tsx
+++ src/components/layout/guild/guild-sidebar.tsx
@@ -1,23 +1,26 @@
-import { Params } from '@/app/[locale]/dash/guilds/[guild]/features/[feature]/page';
-import { FaChevronLeft as ChevronLeftIcon } from 'react-icons/fa';
+'use client';
+
 import { SidebarItem } from '../sidebar/SidebarItem';
 import { Button, Separator } from '@/components/ui';
+import { FaChevronLeft } from 'react-icons/fa';
+import { usePathname } from 'next/navigation';
 import { useGuildPreview } from '@/api/hooks';
 import { getFeatures } from '@/utils/common';
-import { useRouter } from 'next/router';
 
 export function InGuildSidebar() {
-	const router = useRouter();
-	const { guild: guildId, feature: activeId } = router.query as Params;
+	const path = usePathname().split('/');
+	const guildId = path[2];
+	const activeId = path[4];
+
 	const { guild } = useGuildPreview(guildId);
 
 	return (
 		<div className='flex flex-col gap-2 p-3 text-white'>
-			<a className='flex items-center cursor-pointer mb-2' href={`/dash/guilds/${guildId}`}>
+			<a className='flex items-center cursor-pointer mb-2' href={`/guilds/${guildId}`}>
 				<Button className='hidden xl:block card-background rounded-2xl'>
-					<ChevronLeftIcon className='align-middle' />
+					<FaChevronLeft className='align-middle' />
 				</Button>
-				<h1 className='font-semibold ml-2'>
+				<h1 className='font-semibold ml-2 text-white'>
 					{guild?.name}
 				</h1>
 			</a>
@@ -31,7 +34,7 @@ export function InGuildSidebar() {
 						name={feature.name}
 						icon={feature.icon}
 						active={activeId === feature.id}
-						href={`/dash/guilds/${guildId}/features/${feature.id}`}
+						href={`/guilds/${guildId}/features/${feature.id}`}
 					/>
 				))}
 			</div>
