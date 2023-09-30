d7165d79b58544cb327924673b9522f294eb752a
scrappie <notscrappie@proton.me>
2023-09-30 15:50:50 +0300

    refactor(config): removed chakra ui icons

--- src/config/features.tsx
+++ src/config/features.tsx
@@ -1,4 +1,3 @@
-import { Icon } from '@chakra-ui/react';
 import { MdMessage } from 'react-icons/md';
 import { GiTalk } from 'react-icons/gi';
 import { FeaturesConfig } from './types';
@@ -16,49 +15,49 @@ export const features: FeaturesConfig = {
 	antiphishing: {
 		name: 'Anti-Phishing',
 		description: 'Protects your server against scammers.',
-		icon: <Icon as={GiTalk} />,
+		icon: <GiTalk />,
 		useRender: useAntiPhishFeature,
 	},
 	confessions: {
 		name: 'Confessions',
 		description: 'Sends an anonymous message to a designated channel.',
-		icon: <Icon as={GiTalk} />,
+		icon: <GiTalk />,
 		useRender: useConfessionSystem,
 	},
 	goodbye: {
 		name: 'Goodbye',
 		description: 'Sends a custom message when a user joins the server.',
-		icon: <Icon as={MdMessage} />,
+		icon: <MdMessage />,
 		useRender: useGoodbyeSystem,
 	},
 	logs: {
 		name: 'Logs',
 		description: 'Logs every single mod(s) action / server change to a designated channel.',
-		icon: <Icon as={GiTalk} />,
+		icon: <GiTalk />,
 		useRender: useLogsSystem,
 	},
 	levelling: {
 		name: 'Levelling',
 		description: 'Makes chatting more rewarding by introducing an XP system.',
-		icon: <Icon as={GiTalk} />,
+		icon: <GiTalk />,
 		useRender: useLevellingSystem,
 	},
 	tickets: {
 		name: 'Tickets',
 		description: 'Makes chatting more rewarding by introducing an XP system.',
-		icon: <Icon as={GiTalk} />,
+		icon: <GiTalk />,
 		useRender: useTicketSystem,
 	},
 	verification: {
 		name: 'Verification',
 		description: 'Sends a custom message when a user joins the server',
-		icon: <Icon as={MdMessage} />,
+		icon: <MdMessage />,
 		useRender: useVerifySystem,
 	},
 	welcome: {
 		name: 'Welcome',
 		description: 'Sends a custom message when a user joins the server',
-		icon: <Icon as={MdMessage} />,
+		icon: <MdMessage />,
 		useRender: useWelcomeFeature,
 	},
 };
