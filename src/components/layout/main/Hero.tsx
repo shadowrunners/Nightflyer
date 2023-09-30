448362dcf939f0e8719a20ba8ac85e79896cafc4
scrappie <notscrappie@proton.me>
2023-09-30 13:10:23 +0300

    fix(layout): no more key warnings on main page

--- src/components/layout/main/Hero.tsx
+++ src/components/layout/main/Hero.tsx
@@ -16,17 +16,19 @@ const Hero = () => {
 	}, []);
 
 	return (
-		<section id="home" className={'flex md:flex-row flex-col h-screen text-center'}>
+		<section key="home_section" id="home" className={'flex md:flex-row flex-col h-screen text-center'}>
 			<Image
 				src='https://res.cloudinary.com/shadowrunners/image/upload/q_auto/evl1_idtuxu.webp'
 				fill={true}
+				priority={true}
 				alt='coolbg'
 				className='absolute inset-0 object-cover opacity-40 z-0'
 			/>
-			<div className="absolute bottom-0 h-[270px] w-full xl:left-[0px] bg-gradient-to-b from-transparent to-black" />
+			<div key='div_gradient_black' className="absolute bottom-0 h-[270px] w-full xl:left-[0px] bg-gradient-to-b from-transparent to-black" />
 
 			<div
 				className={'flex-1 flex items-center justify-center xl:mb-[140px] flex-col xl:px-0 sm:px-16 px-6 xl:mt-[-250px] xs:mt-[-350px] relative text-center'}
+				key='div_content_hero'
 			>
 				<AnimatePresence>
 					<motion.h1
@@ -36,6 +38,7 @@ const Hero = () => {
 						animate={ isElementInView ? 'visible' : 'hidden' }
 						transition={{ delay: 1.0 }}
 						className={`${styles.heading2} text-center`}
+						key='hero_main_heading'
 					>
 						{translation('hero_main1')} <span className='text-gradient'>{translation('hero_main2')}</span> {translation('hero_main3')}
 					</motion.h1>
@@ -46,6 +49,7 @@ const Hero = () => {
 						animate={ isElementInView ? 'visible' : 'hidden' }
 						transition={{ delay: 1.3 }}
 						className={`${styles.paragraph} max-w-[470px] mt-5`}
+						key='hero_main_paragraph'
 					>
 						{translation('hero_punchline')}
 					</motion.p>
