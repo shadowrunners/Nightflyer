7d421b2b8f127b8089cd13514b69f1a07937208f
scrappie <notscrappie@proton.me>
2023-10-30 14:25:40 +0200

    refactor: removed route handlers

--- src/app/api/[locale]/guilds/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function GET(request: Request) {
-	console.log('Route pinged. [GUILDS]')
-	return new Response('Response');
-}
--- src/app/api/route.ts
+++
@@ -1,5 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-	return NextResponse.redirect('https://www.youtube.com/watch?v=SXRteMSSZ14');
-}
