9985120936f52ec742a59f333e2faa0df533f76a
scrappie <notscrappie@proton.me>
2023-10-30 17:24:03 +0200

    refactor(config): removed filter function

--- src/config/common.tsx
+++ src/config/common.tsx
@@ -1,12 +1,6 @@
-import { PermissionFlags } from '@/types/types';
-import { AppConfig } from './types';
-
-export const config: AppConfig = {
+// This will be removed in the final version.
+export const config = {
 	name: 'Evelyn',
 	inviteUrl:
 		'https://discord.com/oauth2/authorize?client_id=1104526040656785418&scope=bot',
-	guild: {
-		// filter guilds that user has no permissions to manage it
-		filter: (guild) => (Number(guild.permissions) & PermissionFlags.ADMINISTRATOR) !== 0,
-	},
 };
