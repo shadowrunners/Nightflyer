d5438f50e2d8412b3bcb893d9558fd84bd5730dd
scrappie <notscrappie@proton.me>
2023-10-30 20:41:10 +0200

    fix(features): missed an import

--- src/components/homepage/Features.tsx
+++ src/components/homepage/Features.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { styles } from '@/utils/utils';
+import { styles } from '@/utils/util';
 import { motion } from 'framer-motion';
 
 const Features = () => {
