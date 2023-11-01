7f04860d3bf4b8383373b8ebaf6f855741ea760c
scrappie <notscrappie@proton.me>
2023-11-01 16:40:05 +0200

    style(utils): eslint magic

--- src/utils/sidebar.tsx
+++ src/utils/sidebar.tsx
@@ -2,11 +2,11 @@ import { SidebarItemInfo } from '@/utils/router';
 import { MdDashboard } from 'react-icons/md';
 
 const items: SidebarItemInfo[] = [
-    {
-        name: <h1>Home</h1>,
-        path: '/home',
-        icon: <MdDashboard />,
-    },
+	{
+		name: <h1>Home</h1>,
+		path: '/home',
+		icon: <MdDashboard />,
+	},
 ];
 
 export default items;
