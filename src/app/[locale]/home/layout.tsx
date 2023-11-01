470a62530c357cb2dcc8c5551fa150e6d0adc541
scrappie <notscrappie@proton.me>
2023-11-01 14:33:07 +0200

    fix(home): fixed layout on mobile

--- src/app/[locale]/home/layout.tsx
+++ src/app/[locale]/home/layout.tsx
@@ -18,7 +18,7 @@ export default function HomeLayout({ children }: { children: ReactNode }) {
 
 			<QueryStatus query={query} loading={<LoadingPanel />} error="Couldn't fetch the necessary data about the user.">
 				<div className='flex relative flex-col h-[100%] overflow-auto w-full max-w-[100%] max-h-[100%] xl:w-[calc(100%-290px)] card-background'>
-					<div className='top-0 mx-auto max-w-[1200px] w-full sm:pt-[16px] sm:px-[30px]'>
+					<div className='top-0 mx-auto max-w-[1200px] w-full sm:pt-[16px] sm:px-[30px] px-9 pt-[16px]'>
 						<Navbar />
 					</div>
 					<div className='mx-auto w-full max-w-[1200px] flex-1 my-0 px-[24px] sm:px-[30px]'>
