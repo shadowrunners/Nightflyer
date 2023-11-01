b58be52058a75a08db79d4dfd406b22adc8e72d1
scrappie <notscrappie@proton.me>
2023-11-01 17:27:29 +0200

    refactor(types): removed unused types

--- src/types/formTypes.ts
+++ src/types/formTypes.ts
@@ -1,14 +1,4 @@
-import type { FieldValues, Path, UseControllerProps, FieldPathByValue } from 'react-hook-form';
-import type { ReactElement, ReactNode } from 'react';
-import type { Override } from '@/types/types';
-
-type FormProps = {
-    required?: boolean;
-    error?: string;
-    label?: string | ReactNode;
-    description?: string | ReactNode;
-    children: ReactNode;
-};
+import type { ReactElement } from 'react';
 
 type SubmitFn<T> = (data: FormData | string) => Promise<T>;
 
@@ -25,25 +15,3 @@ export type UseFormRenderResult = {
     component: ReactElement;
 };
 
-type ControlledInputProps<
-    T,
-    TFieldValue extends FieldValues,
-    TName extends Path<TFieldValue>
-> = Override<
-    T,
-    {
-        control: Omit<FormProps, 'error' | 'children'>;
-        controller: UseControllerProps<TFieldValue, TName>;
-    }
->;
-
-export type ControlledInput<Props, V = unknown> = <
-    TFieldValues extends FieldValues,
-    TName extends FieldPathByValue<TFieldValues, V>
->(
-    props: ControlledInputProps<Props, TFieldValues, TName>
-) => ReactElement;
-
-export type WithControl<T> = T & {
-    control: Omit<FormProps, 'children'>;
-};
