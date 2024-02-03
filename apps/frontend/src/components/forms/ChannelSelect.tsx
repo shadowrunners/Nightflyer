81029817fc0f6571306b61fd10896f80d4c36da6
scrappie <notscrappie@proton.me>
2024-02-04 01:39:16 +0200

    refactor(frontend): optimized channel select

--- apps/frontend/src/components/forms/ChannelSelect.tsx
+++ apps/frontend/src/components/forms/ChannelSelect.tsx
@@ -25,7 +25,7 @@ import {
 	SelectValue,
 } from '@UI';
 import { useGuildChannelsQuery } from '@Hooks';
-import { usePathname } from 'next/navigation';
+import { useGuildId } from '@/utils/util';
 import { useForm } from 'react-hook-form';
 import { useMemo } from 'react';
 
@@ -35,21 +35,16 @@ export const ChannelSelectForm: ControlledInput<Omit<SelectMenuProps, 'value' | 
 	...props
 }) => {
 	const form = useForm();
-	const guild = usePathname().split('/')[3];
+	const guild = useGuildId();
 
-	const channelsQuery = useGuildChannelsQuery(guild);
-	const isLoading = channelsQuery.isLoading;
+	const { data, isLoading } = useGuildChannelsQuery(guild);
 
-	const finalChannels = useMemo(() => {
-		return channelsQuery.data?.filter((channel) => channel.type !== ChannelTypes.GUILD_VOICE && channel.type !== ChannelTypes.GUILD_STAGE_VOICE && channel.type !== ChannelTypes.GUILD_CATEGORY)
-	}, [channelsQuery]);
-
-	const selected = useMemo(() => {
-		return props.defaultValue !== undefined ? finalChannels?.find((c) => c.id === props.defaultValue?.toString()) : null;
-	}, [props.defaultValue, finalChannels]);
+	const options = useMemo(() => {
+		return data?.filter((channel) => channel.type === ChannelTypes.GUILD_TEXT);
+	}, [data]);
 
 	return (
-		<div className='grid gap-3'>
+		<div className={`grid gap-3 ${props}`}>
 			<div className="flex flex-col width-[100%] relative border-r-3xl p-5 shadow black2 rounded-3xl">
 				<label className="block text-start mr-3 transition-all duration-300 opacity-100 text-base font-medium mb-0">
 					<h2 className="text-2xl font-semibold">{control.label}</h2>
@@ -57,31 +52,33 @@ export const ChannelSelectForm: ControlledInput<Omit<SelectMenuProps, 'value' | 
 				</label>
 				<Spacer />
 				<Form {...form}>
-					<form className='text-white'>
-						<FormField
-							control={controller.control}
-							name={controller.name}
-							render={({ field }) => (
+					<FormField
+						control={controller.control}
+						name={controller.name}
+						render={({ field }) => {
+							const selected = options?.find((c) => c.id === field.value);
+
+							return (
 								<FormItem className='text-white'>
 									<Select value={field.value} onValueChange={field.onChange} disabled={isLoading}>
 										<FormControl>
 											<SelectTrigger>
-												<SelectValue placeholder='placeholder' defaultValue={selected?.id} />
+												<SelectValue placeholder='Select a channel.' defaultValue={selected?.id} key={selected?.id} />
 											</SelectTrigger>
 										</FormControl>
 										<SelectContent className='black2 text-white'>
 											<SelectGroup className='font-poppins m-2'>
 												<SelectLabel>Text Channels</SelectLabel>
-												{finalChannels?.map((channel) => (
-													<SelectItem className='font-poppins' value={channel.id.toString()}>{channel.name}</SelectItem>
+												{options?.map((channel) => (
+													<SelectItem className='font-poppins' value={channel.id} key={channel.id}>{channel.name}</SelectItem>
 												))}
 											</SelectGroup>
 										</SelectContent>
 									</Select>
 								</FormItem>
-							)}
-						/>
-					</form>
+							);
+						}}
+					/>
 				</Form>
 			</div>
 		</div>
@@ -94,25 +91,17 @@ export const MultiChannelSelect = ({
 	value: SelectMenuOptionArray | string[];
 	onChange: (channel: SelectMenuOptionArray) => void;
 }) => {
-	const guild = usePathname().split('/')[3];
-	const channelsQuery = useGuildChannelsQuery(guild);
-	const isLoading = channelsQuery.isLoading;
+	const guild = useGuildId();
+	const { data, isLoading } = useGuildChannelsQuery(guild);
 
 	const options = useMemo(() => {
-		return channelsQuery.data?.map((channel) => ({ label: channel.name, value: channel.id }));
-	}, [channelsQuery.data]);
+		const channels = data?.filter((channel) => channel.type === ChannelTypes.GUILD_TEXT);
+		return channels?.map((channel) => ({ label: channel.name, value: channel.id }));
+	}, [data]);
 
 	const selected = useMemo(() => {
-		// handles cases where value is an array of strings
-		if (value.length > 0 && typeof value[0] === 'string') {
-			return options?.filter((channel) => (value as string[]).includes(channel.value)) ?? [];
-		}
-		// handles cases where value is an array of labels and values. without this, you'll get empty values.
-		// was a fucking pain in the ass to figure out. fuck this.
-		else {
-			const mappedValues = (value as SelectMenuOptionArray).map((channel) => channel.value) ?? [];
-			return options?.filter((channel) => mappedValues.includes(channel.value)) ?? [];
-		}
+		const selectedValues = new Set(value.map((channel) => typeof channel === 'string' ? channel : channel?.value));
+		return options?.filter((channel) => selectedValues.has(channel?.value));
 	}, [options, value]);
 
 	return (
@@ -122,7 +111,7 @@ export const MultiChannelSelect = ({
 			value={selected}
 			placeholder='Select a channel.'
 			options={options}
-			onChange={(e) => e !== null && onChange((e as SelectMenuOptionArray))}
+			onChange={(e) => onChange((e as SelectMenuOptionArray))}
 			{...rest}
 		/>
 	);
@@ -144,17 +133,15 @@ export const MultiChannelSelectForm: ControlledInput<Omit<SelectMenuProps, 'valu
 				</label>
 				<Spacer />
 				<Form {...form}>
-					<form className='text-white'>
-						<FormField
-							control={controller.control}
-							name={controller.name}
-							render={({ field }) => (
-								<FormItem>
-									<MultiChannelSelect {...field} {...props} />
-								</FormItem>
-							)}
-						/>
-					</form>
+					<FormField
+						control={controller.control}
+						name={controller.name}
+						render={({ field }) => (
+							<FormItem>
+								<MultiChannelSelect {...field} {...props} />
+							</FormItem>
+						)}
+					/>
 				</Form>
 			</div>
 		</div>
