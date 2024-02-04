2bfdbd4cc961afb9665bf653aaf69a8ca7281882
scrappie <notscrappie@proton.me>
2024-02-04 02:24:40 +0200

    refactor(frontend): removed bg from loading panel

--- apps/frontend/src/components/panels/LoadingPanel.tsx
+++ apps/frontend/src/components/panels/LoadingPanel.tsx
@@ -5,7 +5,7 @@ export const LoadingPanel = () => {
 	const t = useTranslations();
 
 	return (
-		<div className='flex items-center justify-center flex-col card-background w-full h-full'>
+		<div className='flex items-center justify-center flex-col w-full h-full'>
 			<div className='flex items-center flex-col gap-2.5'>
 				<LuLoader2 className='animate-spin w-[45px] h-[45px] text-white' />
 				<h1 className='text-white'>{t('loading')}</h1>
