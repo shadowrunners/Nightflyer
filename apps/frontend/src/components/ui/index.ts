0c19f6f8075f85099fbd08fa03c7f5a7da67272a
scrappie <notscrappie@proton.me>
2024-02-04 02:25:00 +0200

    refactor(frontend): removed banner & added select & gselect to index

--- apps/frontend/src/components/ui/index.ts
+++ apps/frontend/src/components/ui/index.ts
@@ -1,11 +1,10 @@
 import { Avatar, AvatarImage, AvatarFallback } from './avatar';
 import { Button } from './button';
-import { Banner } from './gbanner';
 import { Card, CardFooter, CardContent, CardTitle, CardHeader, CardDescription } from './card';
 import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuLabel, DropdownMenuItem, DropdownMenuSeparator } from './dropdown-menu';
 import { Form, FormDescription, FormMessage, FormField, FormItem, FormLabel, FormControl } from './form';
 import { Input } from './input';
-import { GuildSelect } from './GuildSelect';
+import { GuildSelect } from './guildselect';
 import { Label } from './label';
 import { Popover, PopoverContent, PopoverTrigger } from './popover';
 import { Profile } from './profile';
@@ -13,6 +12,7 @@ import { SelectMenu } from './selectmenu';
 import { ScrollArea } from './scroll-area';
 import { Sidebar, MobileSidebar } from './sidebar';
 import { Separator } from './separator';
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectSeparator, SelectTrigger, SelectValue } from './select';
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetDescription, SheetFooter, SheetTrigger } from './sheet';
 import { Skeleton } from './skeleton';
 import { Spacer } from './spacer';
@@ -23,7 +23,6 @@ export {
 	AvatarImage,
 	AvatarFallback,
 	Button,
-	Banner,
 	Card,
 	CardFooter,
 	CardHeader,
@@ -62,6 +61,14 @@ export {
 	SheetTitle,
 	SheetDescription,
 	SheetFooter,
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectLabel,
+	SelectSeparator,
+	SelectTrigger,
+	SelectValue,
 	Skeleton,
 	Spacer,
 	Textarea,
