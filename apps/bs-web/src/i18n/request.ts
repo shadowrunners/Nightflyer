64d2ba11177d4705d955a88e5978353b7f7d3a7c
scrappie <notscrappie@proton.me>
2025-07-22 22:15:57 +0300

    refactor(bs-web): fetch locale cookie in request.ts

--- apps/bs-web/src/i18n/request.ts
+++ apps/bs-web/src/i18n/request.ts
@@ -1,12 +1,9 @@
 import { getRequestConfig } from 'next-intl/server';
-import { routing } from './routing';
+import { cookies } from 'next/headers';
 
-export default getRequestConfig(async ({ requestLocale }) => {
-    let locale = await requestLocale;
-
-    if (!locale || !routing.locales.includes(locale as any)) {
-        locale = routing.defaultLocale;
-    };
+export default getRequestConfig(async () => {
+    const jar = await cookies();
+    const locale = jar.get('bs-locale')?.value ?? 'en';
 
     return {
         locale,
