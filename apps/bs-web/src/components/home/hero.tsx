fb424df7aae6a9d967f9ca2d6af8e1b3e8d5e450
scrappie <notscrappie@proton.me>
2025-07-22 22:14:51 +0300

    refactor(bs-web): reworked hero component

--- apps/bs-web/src/components/home/hero.tsx
+++ apps/bs-web/src/components/home/hero.tsx
@@ -1,41 +1,18 @@
 'use client';
 
 import { useTranslations } from 'next-intl';
-import Image from 'next/image';
-
-// TODO: Re-do this completely so it's more modern. Possibly use Aceternity UI.
 
 export function Hero() {
 	const t = useTranslations('home');
 
 	return (
-		<div className='flex h-screen w-full items-center justify-center overflow-hidden px-4 text-center'>
-			<Image
-				src='/images/bf_placeholder.png'
-				fill={true}
-				priority={true}
-				alt='coolbg'
-				className='absolute inset-0 h-full w-full object-cover opacity-40'
-			/>
+		<div className='flex h-screen w-full items-center overflow-hidden px-4 text-left'>
 			<div key='div_gradient_black' className="absolute bottom-0 h-[270px] w-full xl:left-[0px] bg-gradient-to-b from-transparent to-black" />
-			<div className="px-4 py-10 md:py-20 -mt-35">
-
-			</div>
-
-			<div
-				className='flex-1 flex items-center justify-center xl:mb-[140px] flex-col xl:px-0 sm:px-16 px-6 xl:mt-[-250px] xs:mt-[-350px] relative text-center'
-				key='div_content_hero'
-			>
-				<h1
-					className={`text-center text-white`}
-					key='hero_main_heading'
-				>
-					{t('main.hero.main1')} <span className='text-gradient'>{t('main.hero.main2')}</span> {t('main.hero.main3')}
+			<div className='px-4 py-10 md:py-20 -mt-35'>
+				<h1 className='relative z-10 mx-auto max-w-4xl text-2xl font-bold font-sans text-slate-300 md:text-4xl lg:text-7xl'>
+					{t('main.hero.main')}
 				</h1>
-				<p
-					className={`max-w-[470px] mt-5`}
-					key='hero_main_paragraph'
-				>
+				<p className='relative mx-1 z-10 max-w-xl py-4 text-lg text-left font-normal text-neutral-400'>
 					{t('main.hero.punchline')}
 				</p>
 			</div>
